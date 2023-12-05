a3bf4f6b42b4fe03e67011b463d292ad7e3b89d7
sbk283 <sbk283@naver.com>
2023-12-05 11:30:44 +0900

    js안쓰고 검색기능 구현

--- src/main/java/com/article1/article/article/ArticleController.java
+++ src/main/java/com/article1/article/article/ArticleController.java
@@ -25,11 +25,10 @@ public class ArticleController {
     private final UserService userService;
 
     @GetMapping("/list")
-    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
-                       @RequestParam(value = "kw", defaultValue = "") String kw) {
+    public String list(Model model,  @RequestParam(value="page", defaultValue="0") int page, @RequestParam(value = "kw", defaultValue = "") String kw) {
         Page<Article> paging = this.articleService.getList(page, kw);
         model.addAttribute("paging", paging);
-        model.addAttribute("kw", kw);
+
         return "article_list";
     }
 
